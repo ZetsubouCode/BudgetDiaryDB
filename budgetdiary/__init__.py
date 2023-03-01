eaff6a17d0fdfc1b0f960043b127382002596c98
ZetsubouCode <kignatius80@gmail.com>
2023-03-01 08:21:36 +0700

    Implement all sub route

--- budgetdiary/__init__.py
+++ budgetdiary/__init__.py
@@ -3,10 +3,38 @@ from fastapi import FastAPI
 app = FastAPI()
 
 from .route.User import subroute as user_route
-# from .route.Income import subroute as income_route
-
 app.include_router(
     user_route,
     prefix='/user',
     tags=['User']
 )
+
+from .route.Saving import subroute as saving_route
+app.include_router(
+    saving_route,
+    prefix='/saving',
+    tags=['Saving']
+)
+
+from .route.Budget import subroute as budget_route
+app.include_router(
+    budget_route,
+    prefix='/budget',
+    tags=['Budget']
+)
+
+from .route.Income import subroute as income_route
+app.include_router(
+    income_route,
+    prefix='/income',
+    tags=['Income']
+)
+
+from .route.Outcome import subroute as outcome_route
+app.include_router(
+    outcome_route,
+    prefix='/outcome',
+    tags=['Outcome']
+)
+
+# from .route.Income import subroute as income_route
