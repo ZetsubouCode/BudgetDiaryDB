b2f5c4d519423cb6bbe2cfec14cd59a94d42fca4
Kevin Ignatius <53389426+ZetsubouCode@users.noreply.github.com>
2023-03-06 09:45:46 +0700

    Update User.py
    Adding some handling to anticipate NULL value

--- budgetdiary/route/User.py
+++ budgetdiary/route/User.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Form
+from typing import Optional
 
 from ..controller.User import User as UserController
 from ..model.response import BaseResponse
@@ -7,13 +8,13 @@ from ..utils import Debug
 subroute = APIRouter()
 
 @subroute.post("/login", response_model=BaseResponse)
-async def login(discord_username:str = Form(...), password:str = Form(...)):
+async def login(discord_username:str = Form(...), pin:str = Form(...)):
     debug_identifier = "User|login"
     try:
-        user = await UserController.authenticate(discord_username, password)
+        user = await UserController.authenticate(discord_username, pin)
         if not user:
-            Debug.msg(debug_identifier, "Username or password is incorrect")
-            return BaseResponse(**{"status": "Username or password is incorrect"})
+            Debug.msg(debug_identifier, "Username or pin is incorrect")
+            return BaseResponse(**{"status": "Username or pin is incorrect"})
 
         return BaseResponse(**{"status": "Success", "content": user})
 
@@ -52,7 +53,7 @@ async def get_all():
         return BaseResponse(**{"status": "Server error"})
 
 @subroute.post("/add", response_model=BaseResponse)
-async def add(discord_username:str = Form(...), pin:str = Form(...)):
+async def add(discord_username:str = Form(...), pin:Optional[str] = Form(None)):
     debug_identifier = "UserRoute|add"
     try:
         if await UserController.get_by_username(discord_username):
@@ -72,7 +73,7 @@ async def add(discord_username:str = Form(...), pin:str = Form(...)):
         return BaseResponse(**{"status": "Server error"})
 
 @subroute.put("/update/id/{id}", response_model=BaseResponse)
-async def update_by_id(id: int, discord_username:str = Form(...), pin: str = Form(...), balance:float = Form(...),):
+async def update_by_id(id: int, discord_username:Optional[str] = Form(None), pin: Optional[str] = Form(None), balance:float = Form(...)):
                       
     debug_identifier = "UserRoute|update_by_id"
     try:
@@ -80,10 +81,11 @@ async def update_by_id(id: int, discord_username:str = Form(...), pin: str = For
         if not updated_user:
             Debug.msg(debug_identifier, "User does not exist")
             return BaseResponse(**{"status": "User does not exist"})
-
-        check_if_exist = await UserController.get_by_id(id)
-        if check_if_exist and check_if_exist.discord_username != updated_user.discord_username:
-            return BaseResponse(**{"status": "Username already exist"})
+        
+        if discord_username:
+            check_if_exist = await UserController.get_by_id(id)
+            if check_if_exist and check_if_exist.discord_username != updated_user.discord_username:
+                return BaseResponse(**{"status": "Username already exist"})
 
         updated_user.discord_username = discord_username
         updated_user.pin = pin
@@ -101,7 +103,7 @@ async def update_by_id(id: int, discord_username:str = Form(...), pin: str = For
 
 
 @subroute.put("/update/discord_username/{discord_username}", response_model=BaseResponse)
-async def update_by_username(discord_username: str, pin:str = Form(...),balance:float = Form(...)):
+async def update_by_username(discord_username: str, pin:Optional[str] = Form(None), balance:float = Form(...)):
 
     debug_identifier = "UserRoute|update_by_name"
     try:
@@ -149,18 +151,18 @@ async def delete_by_id(id: int):
 
 
 @subroute.delete("/delete/discord_username/{discord_username}", response_model=BaseResponse)
-async def delete_by_username(target_name: str):
+async def delete_by_username(target_username: str):
     debug_identifier = "UserRoute|delete_by_username"
     try:
-        user = await UserController.get_by_username(target_name)
+        user = await UserController.get_by_username(target_username)
         if not user:
             Debug.msg(debug_identifier, "User does not exist")
             return BaseResponse(**{"status": "User does not exist"})
 
-        await UserController.delete_by_username(target_name)
-        if not user:
-            Debug.msg(debug_identifier, "User not found")
-            return BaseResponse(**{"status": "User not found"})
+        success_delete = await UserController.delete_by_username(target_username)
+        if not success_delete:
+            Debug.msg(debug_identifier, "Failed to delete data")
+            return BaseResponse(**{"status": "Error", "content": "Failed to delete data"})
 
         return BaseResponse(**{"status": "Success", "content": "Delete action successful"})
 
