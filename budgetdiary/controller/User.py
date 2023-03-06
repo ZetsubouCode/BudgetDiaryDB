22cb225dd5a02b093e4eaa66d68fa67311f59687
Kevin Ignatius <53389426+ZetsubouCode@users.noreply.github.com>
2023-03-06 10:04:20 +0700

    Update User.py
    Update documentation

--- budgetdiary/controller/User.py
+++ budgetdiary/controller/User.py
@@ -12,7 +12,7 @@ class User:
     @staticmethod
     async def get_by_id(target_id: int) -> UserModel:
         """
-        Get the first result of User by its id
+        Get the User object by its id
         @param target_id: The id of the User data
         @return: User object
         """
@@ -24,7 +24,7 @@ class User:
     @staticmethod
     async def get_by_username(target_username: str) -> UserModel:
         """
-        Get the first result of User by its username
+        Get the User object by its username
         @param target_id: The id of the User data
         @return: User object
         """
@@ -36,8 +36,8 @@ class User:
     @staticmethod
     async def get_all() -> List[UserModel]:
         """
-        Get all result of User data
-        @return: List of User object
+        Get all the User objects
+        @return: List of User objects
         """
         with get_session() as session:
             user = session.query(UserModel).all()
@@ -46,9 +46,10 @@ class User:
     @staticmethod
     async def add(discord_username: str, pin: str, balance: float) -> UserModel:
         """
-        Create User object and add it to the database
-        @param last_layer: User last_layer
-        @param last_room: User last_room
+        Add a new User object to the database
+        @param discord_username: Discord username of the user
+        @param pin: PIN of the user
+        @param balance: balance of the user
         @return: User object
         """
         with get_session() as session:
@@ -66,10 +67,10 @@ class User:
     async def update_by_id(target_id: int,
                            new_obj: UserModel) -> UserModel:
         """
-        Update User object that have the specific id
-        @param taget_id: User id
-        @param new_obj: User User new set of data
-        @return: User object
+        Update the User object by its id
+        @param target_id: Id of the User object to update
+        @param new_obj: New User object data
+        @return: Updated User object
         """
         with get_session() as sess:
             sess.query(UserModel).filter_by(id=int(target_id)).update({
@@ -80,44 +81,50 @@ class User:
             sess.commit()
         return new_obj
 
-@staticmethod
-async def delete_by_id(target_id: int):
-    """
-    Delete User object that have the specific id
-    @param taget_id: User id
-    """
-    try:
-        with get_session() as sess:
-            try:
-                sess.query(UserModel).filter_by(
-                    id=int(target_id)).delete()
-                sess.flush()
+    @staticmethod
+    async def delete_by_id(target_id: int):
+        """
+        Delete the User object by its id
+        @param target_id: Id of the User object to delete
+        @return: True if User object is deleted, False otherwise
+        """
+        try:
+            with get_session() as sess:
+                try:
+                    sess.query(UserModel).filter_by(
+                        id=int(target_id)).delete()
+                    sess.flush()
 
-            except Exception as e:
-                sess.rollback()
-                Debug.msg("UserController|delete_by_id",
-                          "Failed to Delete {}".format(e),
-                          DebugLevel.WARNING)
-                return False
+                except Exception as e:
+                    sess.rollback()
+                    Debug.msg("UserController|delete_by_id",
+                              "Failed to Delete {}".format(e),
+                              DebugLevel.WARNING)
+                    return False
 
-            try:
-                sess.commit()
-            except Exception as e:
-                sess.rollback()
-                Debug.msg("UserController|delete_by_id",
-                          "Failed to commit transaction: {}".format(e),
-                          DebugLevel.ERROR)
-                return False
-                
-        return True
+                try:
+                    sess.commit()
+                except Exception as e:
+                    sess.rollback()
+                    Debug.msg("UserController|delete_by_id",
+                              "Failed to commit transaction: {}".format(e),
+                              DebugLevel.ERROR)
+                    return False
 
-    except Exception as e:
-        Debug.msg("UserController|delete_by_id",
-                  "Exception Raised {}".format(e), DebugLevel.ERROR)
-        return False
+            return True
+
+        except Exception as e:
+            Debug.msg("UserController|delete_by_id",
+                      "Exception Raised {}".format(e), DebugLevel.ERROR)
+            return False
             
     @staticmethod
     async def encrypt_pin(pin:str) -> str:
+        """
+        Encrypts the given PIN with SHA256 algorithm and returns the encrypted value.
+        @param pin: The plain text PIN to be encrypted.
+        @return: The encrypted value of the given PIN.
+        """
         encoded = (pin+os.environ.get("SALT")).encode()
         result = hashlib.sha256(encoded)
         
@@ -125,6 +132,12 @@ async def delete_by_id(target_id: int):
 
     @staticmethod
     async def authenticate(username:str, pin:str) -> bool :
+        """
+        Authenticates the user with the given username and PIN.
+        @param username: The username of the user to be authenticated.
+        @param pin: The PIN of the user to be authenticated.
+        @return: True if the user is authenticated, False otherwise.
+        """
         log_identifier = "UserController|check_pin"
 
         result = await User.encrypt_pin(pin)
