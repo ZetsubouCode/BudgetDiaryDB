c91d502b3e650062772ee5455013246da0126f87
Kevin Ignatius <53389426+ZetsubouCode@users.noreply.github.com>
2023-03-07 10:48:55 +0700

    Update run.sh
    edit the username

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-cd /home/u6969690/public_html/budget_diary
+cd /home/u1572803/public_html/budget_diary
 ~/virtualenv/public_html/budget_diary/3.10/bin/python3 -m uvicorn budgetdiary:app --port 55002
