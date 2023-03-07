b5270b7ed022686cdad482b9ba4d6a7a9aa6750f
Kevin Ignatius <53389426+ZetsubouCode@users.noreply.github.com>
2023-03-07 13:54:12 +0700

    Update run.sh
    fix typo version

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 cd /home/u1572803/public_html/budget_diary
-~/virtualenv/public_html/budget_diary/3.7.12/bin/python3 -m uvicorn budgetdiary:app --port 55002
+~/virtualenv/public_html/budget_diary/3.7/bin/python3 -m uvicorn budgetdiary:app --port 55002
