ffa943cdfd4c33085de657852c4f13cef64d0135
Kevin Ignatius <53389426+ZetsubouCode@users.noreply.github.com>
2023-03-07 10:48:00 +0700

    Update run.sh
    Adjust content for niagahoster deployment process

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-./venv/Scripts/python.exe -m uvicorn budgetdiary:app --host 0.0.0.0 --port 55001 --reload
+cd /home/u6969690/public_html/budget_diary
+~/virtualenv/public_html/budget_diary/3.10/bin/python3 -m uvicorn budgetdiary:app --port 55002
