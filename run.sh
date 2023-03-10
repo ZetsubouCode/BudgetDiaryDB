a408ec077411167b646fb1fd121ff3b034f11c75
Kevin Ignatius <53389426+ZetsubouCode@users.noreply.github.com>
2023-03-10 17:02:17 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-cd /home/u1572803/public_html/budget_diary
-~/virtualenv/public_html/budget_diary/3.7/bin/python3 -m uvicorn budgetdiary:app --port 55002
+cd /home/u1572803/public_html/BudgetDiaryDB
+~/virtualenv/public_html/BudgetDiaryDB/3.10/bin/python3 -m uvicorn budgetdiary:app --port 55002
