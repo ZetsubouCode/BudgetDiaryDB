9ff0252f69bf5bc1145fbfa12b670a57df5ee0ce
ZetsubouCode <kignatius80@gmail.com>
2023-03-27 05:22:52 +0700

    Updating and fixing User functionality

--- budgetdiary/controller/User.py
+++ budgetdiary/controller/User.py
@@ -1,6 +1,5 @@
+import hashlib, os
 from typing import List
-from datetime import date
-from sqlalchemy.sql import func
 from sqlalchemy.orm import joinedload
 from ..__database import get_session
 from ..model.database import User as UserModel
@@ -19,6 +18,17 @@ class User:
 
         return user
 
+    async def get_by_discord_username(target_username: str) -> UserModel:
+        """
+        Get the first result of User by its id
+        @param target_id: The id of the User data
+        @return: User object
+        """
+        with get_session() as session:
+            data = session.query(UserModel).filter_by(discord_username=target_username).first()
+
+        return data
+    
     @staticmethod
     async def get_all() -> List[UserModel]:
         """
@@ -26,12 +36,11 @@ class User:
         @return: List of User object
         """
         with get_session() as session:
-            user = session.query(UserModel).options(
-                joinedload(UserModel.user_type)).all()
+            user = session.query(UserModel).all()
         return user
 
     @staticmethod
-    async def add(discord_username: str, pin: str, balance: float) -> UserModel:
+    async def add(discord_username: str, pin: str) -> UserModel:
         """
         Create User object and add it to the database
         @param last_layer: User last_layer
@@ -40,8 +49,7 @@ class User:
         """
         with get_session() as session:
             user = UserModel(discord_username=discord_username,
-                                 pin=pin,
-                                 balance=balance)
+                                 pin=pin)
             session.add(user)
             session.commit()
             session.flush()
@@ -60,6 +68,24 @@ class User:
         """
         with get_session() as sess:
             sess.query(UserModel).filter_by(id=int(target_id)).update({
+                UserModel.discord_username:new_obj.discord_username,
+                UserModel.pin:new_obj.pin,
+                UserModel.balance:new_obj.balance
+            })
+            sess.commit()
+        return new_obj
+    
+    @staticmethod
+    async def update_by_discord_username(target_username: str,
+                           new_obj: UserModel) -> UserModel:
+        """
+        Update User object that have the specific id
+        @param taget_id: User id
+        @param new_obj: User User new set of data
+        @return: User object
+        """
+        with get_session() as sess:
+            sess.query(UserModel).filter_by(id=int(target_username)).update({
                 UserModel.discord_username:new_obj.discord_username,
                 UserModel.pin:new_obj.pin,
                 UserModel.balance:new_obj.balance
@@ -91,3 +117,107 @@ class User:
         except Exception as e:
             Debug.msg("UserController|delete_by_id",
                       "Exception Raised {}".format(e), DebugLevel.ERROR)
+    @staticmethod
+    async def delete_by_discord_username(target_id: int) -> bool:
+        """
+        Delete User object that have the specific id
+        @param taget_id: User id
+        """
+        try:
+            with get_session() as sess:
+
+                try:
+                    rows_affected = sess.query(UserModel).filter_by(
+                        id=target_id).delete()
+                    sess.commit()
+                    sess.flush()
+                    return rows_affected > 0
+
+                except Exception as e:
+                    sess.rollback()
+                    Debug.msg("UserController|delete_by_id",
+                            "Failed to Delete {}".format(e),
+                            DebugLevel.WARNING)
+                    return False
+
+        except Exception as e:
+            Debug.msg("UserController|delete_by_id",
+                    "Exception Raised {}".format(e), DebugLevel.ERROR)
+            return False
+        
+    @staticmethod
+    async def delete_by_discord_username(target_username: str) -> bool:
+        """
+        Delete User object that have the specific discord username
+        @param target_username: User discord username
+        """
+        try:
+            with get_session() as sess:
+
+                try:
+                    rows_affected = sess.query(UserModel).filter_by(
+                        discord_username=target_username).delete()
+                    sess.commit()
+                    sess.flush()
+                    return rows_affected > 0
+
+                except Exception as e:
+                    sess.rollback()
+                    Debug.msg("UserController|delete_by_discord_username",
+                            "Failed to Delete {}".format(e),
+                            DebugLevel.WARNING)
+                    return False
+
+        except Exception as e:
+            Debug.msg("UserController|delete_by_discord_username",
+                    "Exception Raised {}".format(e), DebugLevel.ERROR)
+            return False
+
+    @staticmethod
+    async def update_pin_by_reset(discord_username:str) -> bool:
+            enc_username = await User.encrypt_pin(discord_username)
+            with get_session() as sess:
+                    sess.query(UserModel).filter_by(discord_username=discord_username).update(
+                        {
+                            UserModel.pin: enc_username
+                        }
+                    )
+                    sess.commit()
+                    sess.flush()
+            return True
+    
+    @staticmethod
+    async def update_pin_by_change(discord_username:str, pin: str) -> bool:
+            if not await User.authenticate(discord_username,pin):
+                return False
+            with get_session() as sess:
+                    sess.query(UserModel).filter_by(discord_username=discord_username).update(
+                        {
+                            UserModel.pin: await User.encrypt_pin(pin)
+                        }
+                    )
+                    sess.commit()
+                    sess.flush()
+            return True
+
+    @staticmethod
+    async def encrypt_pin(pin:str) -> str:
+        encoded = (pin+os.environ.get("SALT")).encode()
+        result = hashlib.sha256(encoded)
+        
+        return result.hexdigest()
+
+    @staticmethod
+    async def authenticate(discord_username:str, pin:str) -> bool :
+        log_identifier = "UserController|check_pin"
+
+        result = await User.encrypt_pin(pin)
+        data = await User.get_by_discord_username(discord_username)
+        if not data:
+            Debug.msg(log_identifier, "Data not found", DebugLevel.INFO)
+            return None
+        if result!=data.pin:
+            Debug.msg(log_identifier, "Wrong pin", DebugLevel.INFO)
+            return None
+
+        return data
--- budgetdiary/route/User.py
+++ budgetdiary/route/User.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Form
+from typing import Optional
 
 from ..controller.User import User as UserController
 from ..model.response import BaseResponse
@@ -27,71 +28,90 @@ async def get_by_id(id: int):
     try:
         user = await UserController.get_by_id(id)
         if not user:
-            Debug.msg(debug_identifier, "User not found")
-            return BaseResponse(**{"status": "User not found"})
-        
+            message = "User not found"
+            Debug.msg(debug_identifier, message)
+            return BaseResponse(**{"status": message})
+
         return BaseResponse(**{"status": "Success", "content": user})
 
     except Exception as e:
+        message = "Server error"
         Debug.msg(debug_identifier, "Exception Raised: {}".format(e))
-        return BaseResponse(**{"status": "Server error"})
+        return BaseResponse(**{"status": message})
+
 
 @subroute.get("/all", response_model=BaseResponse)
 async def get_all():
     debug_identifier = "User|get_all"
     try:
-        user = await UserController.get_all()
-        if not user:
-            Debug.msg(debug_identifier, "Data not found")
-            return BaseResponse(**{"status": "Data not found"})
+        users = await UserController.get_all()
+        if not users:
+            message = "Data not found"
+            Debug.msg(debug_identifier, message)
+            return BaseResponse(**{"status": message})
 
-        return BaseResponse(**{"status": "Success", "content": user})
+        return BaseResponse(**{"status": "Success", "content": users})
 
     except Exception as e:
+        message = "Server error"
         Debug.msg(debug_identifier, "Exception Raised: {}".format(e))
-        return BaseResponse(**{"status": "Server error"})
+        return BaseResponse(**{"status": message})
+
 
 @subroute.post("/add", response_model=BaseResponse)
 async def add(discord_username:str = Form(...), pin:str = Form(...)):
     debug_identifier = "UserRoute|add"
     try:
-        if await UserController.get_by_username(discord_username):
-            Debug.msg(debug_identifier, "User already exist")
-            return BaseResponse(**{"status": "User already exist"})
-
-        user = await UserController.add(discord_username,pin)
+        if await UserController.get_by_discord_username(discord_username):
+            message = "User already exists"
+            Debug.msg(debug_identifier, message)
+            return BaseResponse(**{"status": message})
+        
+        encrypted_pin = await UserController.encrypt_pin(pin)
+        user = await UserController.add(discord_username, encrypted_pin)
 
         if not user:
-            Debug.msg(debug_identifier, "User not found")
-            return BaseResponse(**{"status": "User not found"})
+            message = "User not found"
+            Debug.msg(debug_identifier, message)
+            return BaseResponse(**{"status": message})
 
         return BaseResponse(**{"status": "Success", "content": user})
 
     except Exception as e:
+        message = "Server error"
         Debug.msg(debug_identifier, "Exception Raised: {}".format(e))
-        return BaseResponse(**{"status": "Server error"})
+        return BaseResponse(**{"status": message})
+
 
 @subroute.put("/update/id/{id}", response_model=BaseResponse)
-async def update_by_id(id: int, discord_username:str = Form(...), pin: str = Form(...), balance:float = Form(...),):
-                      
+async def update_by_id(id: int, discord_username: Optional[str] = Form(None), 
+                       pin: Optional[str] = Form(None), balance: Optional[float] = Form(None)):
     debug_identifier = "UserRoute|update_by_id"
     try:
         updated_user = await UserController.get_by_id(id)
         if not updated_user:
-            Debug.msg(debug_identifier, "User does not exist")
-            return BaseResponse(**{"status": "User does not exist"})
-
-        check_if_exist = await UserController.get_by_id(id)
+            message = "User does not exist"
+            Debug.msg(debug_identifier, message)
+            return BaseResponse(**{"status": message})
+        
+        check_if_exist = await UserController.get_by_discord_username(discord_username)
         if check_if_exist and check_if_exist.discord_username != updated_user.discord_username:
-            return BaseResponse(**{"status": "Username already exist"})
+            message = "Username already exists"
+            Debug.msg(debug_identifier, message)
+            return BaseResponse(**{"status": message})
 
-        updated_user.discord_username = discord_username
-        updated_user.pin = pin
+        if discord_username:
+            updated_user.discord_username = discord_username
+        if pin:
+            updated_user.pin = pin
+        if balance:
+            updated_user.balance = balance
 
         user = await UserController.update_by_id(id, updated_user)
         if not user:
-            Debug.msg(debug_identifier, "User not found")
-            return BaseResponse(**{"status": "User not found"})
+            message = "User not found"
+            Debug.msg(debug_identifier, message)
+            return BaseResponse(**{"status": message})
 
         return BaseResponse(**{"status": "Success", "content": user})
 
@@ -100,25 +120,34 @@ async def update_by_id(id: int, discord_username:str = Form(...), pin: str = For
         return BaseResponse(**{"status": "Server error"})
 
 
-@subroute.put("/update/discord_username/{discord_username}", response_model=BaseResponse)
-async def update_by_username(discord_username: str, pin:str = Form(...),balance:float = Form(...)):
+@subroute.patch("/update/discord_username/{discord_username}", response_model=BaseResponse)
+async def update_by_username(discord_username: str, new_discord_username:Optional[str] = Form(None), 
+                             pin:Optional[str] = Form(None),balance:Optional[float] = Form(None)):
 
     debug_identifier = "UserRoute|update_by_name"
     try:
-        updated_user = await UserController.get_by_username(discord_username)
+        if not (new_discord_username and pin and balance):
+            message = "Nothing to update"
+            Debug.msg(debug_identifier, message)
+            return BaseResponse(**{"status": message})
+        updated_user = await UserController.get_by_discord_username(discord_username)
         if not updated_user:
-            Debug.msg(debug_identifier, "User does not exist")
-            return BaseResponse(**{"status": "User does not exist"})
+            message = "User does not exist"
+            Debug.msg(debug_identifier, message)
+            return BaseResponse(**{"status": message})
+        if new_discord_username:
+            check_if_exist = await UserController.get_by_discord_username(new_discord_username)
+            if check_if_exist and check_if_exist.discord_username != updated_user.discord_username:
+                message = "User already exist"
+                Debug.msg(debug_identifier, message)
+                return BaseResponse(**{"status": message})
+            updated_user.discord_username = new_discord_username
+        if pin:
+            updated_user.pin = pin
+        if balance:
+            updated_user.balance = balance
 
-        check_if_exist = await UserController.get_by_username(discord_username)
-        if check_if_exist and check_if_exist.discord_username != updated_user.discord_username:
-            return BaseResponse(**{"status": "User already exist"})
-
-        updated_user.discord_username = discord_username
-        updated_user.pin = pin
-        updated_user.balance = balance
-
-        user = await UserController.update_by_username(discord_username, updated_user, balance)
+        user = await UserController.update_by_discord_username(discord_username, updated_user, balance)
         if not user:
             Debug.msg(debug_identifier, "User not found")
             return BaseResponse(**{"status": "User not found"})
@@ -136,12 +165,15 @@ async def delete_by_id(id: int):
     try:
         user = await UserController.get_by_id(id)
         if not user:
-            Debug.msg(debug_identifier, "User does not exist")
-            return BaseResponse(**{"status": "User does not exist"})
+            message = "User does not exist"
+            Debug.msg(debug_identifier, message)
+            return BaseResponse(**{"status": message})
         
         await UserController.delete_by_id(id)
 
-        return BaseResponse(**{"status": "Success", "content": "Delete action successful"})
+        message = "Delete action successful"
+        Debug.msg(debug_identifier, message)
+        return BaseResponse(**{"status": "Success", "content": message})
 
     except Exception as e:
         Debug.msg(debug_identifier, "Exception Raised: {}".format(e))
@@ -152,17 +184,21 @@ async def delete_by_id(id: int):
 async def delete_by_username(target_name: str):
     debug_identifier = "UserRoute|delete_by_username"
     try:
-        user = await UserController.get_by_username(target_name)
+        user = await UserController.get_by_discord_username(target_name)
         if not user:
-            Debug.msg(debug_identifier, "User does not exist")
-            return BaseResponse(**{"status": "User does not exist"})
+            message = "User does not exist"
+            Debug.msg(debug_identifier, message)
+            return BaseResponse(**{"status": message})
 
-        await UserController.delete_by_username(target_name)
-        if not user:
-            Debug.msg(debug_identifier, "User not found")
-            return BaseResponse(**{"status": "User not found"})
+        deleted = await UserController.delete_by_discord_username(target_name)
+        if not deleted:
+            message = "Failed to delete user"
+            Debug.msg(debug_identifier, message)
+            return BaseResponse(**{"status": message})
 
-        return BaseResponse(**{"status": "Success", "content": "Delete action successful"})
+        message = "Delete action successful"
+        Debug.msg(debug_identifier, message)
+        return BaseResponse(**{"status": "Success", "content": message})
 
     except Exception as e:
         Debug.msg(debug_identifier, "Exception Raised: {}".format(e))
